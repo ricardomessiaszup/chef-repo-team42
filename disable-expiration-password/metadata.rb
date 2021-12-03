b1e97bdfba36c863d6badaaee9dbc5b6e1c6594b
Ricardo Messias Ferreira <ricardo.ferreira@zup.com.br>
2021-12-03 16:43:13 -0300

    .

--- disable-expiration-password/metadata.rb
+++ disable-expiration-password/metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'you@example.com'
 license 'All Rights Reserved'
 description 'Installs/Configures disable-expiration-password'
 version '0.1.0'
-chef_version '>= 15.0'
+chef_version '>= 11.0'
 
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
