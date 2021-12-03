e2fa584a01ef66bd4a31f51388da7de941e29f01
Ricardo Messias Ferreira <ricardo.ferreira@zup.com.br>
2021-12-03 17:10:02 -0300

    .

--- disable-expiration-password/recipes/default.rb
+++ disable-expiration-password/recipes/default.rb
@@ -3,7 +3,24 @@
 # Recipe:: disable-expiration-password
 #
 # Copyright:: 2021, Ricardo Messias Ferreira, All Rights Reserved.
+#
+# Disable Expiration Password for User: Ricardo Messias Ferreira
 execute "disable-expiration-password" do
+    command "sudo chage -m 0 -M 99999 -I -1 -E -1 ricardoferreira"
+    action :run
+  end
+# Disable Expiration Password for User: Cleiton Schulz
+  execute "disable-expiration-password" do
     command "sudo chage -m 0 -M 99999 -I -1 -E -1 cleitonschulz"
+    action :run
+  end
+# Disable Expiration Password for User: Gustavo Moura Rezende
+  execute "disable-expiration-password" do
+    command "sudo chage -m 0 -M 99999 -I -1 -E -1 gustavorezende"
+    action :run
+  end
+# Disable Expiration Password for User: Gabriella de Lima Tardivo
+  execute "disable-expiration-password" do
+    command "sudo chage -m 0 -M 99999 -I -1 -E -1 gabriellalima"
     action :run
   end
